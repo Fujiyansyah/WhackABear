7c3e4f9b03a0db00122907141d331804d21ef489
Fujiyansyah <muhammadsyafwan04@gmail.com>
2022-02-05 12:07:48 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const tanah = document.querySelectorAll('.tanah');
+const bear = document.querySelectorAll('.bear');
+const papanSkor = document.querySelector('.papan-skor');
+const pop = document.querySelector('#pop');
+
+let tanahSebelumnya;
+let selesai;
+let skor;
+
+function randomTanah(tanah) {
+  const t = Math.floor(Math.random() * tanah.length);
+  const tRandom = tanah[t];
+  if (tRandom == tanahSebelumnya) {
+    randomTanah(tanah);
+  }
+  tanahSebelumnya = tRandom;
+  return tRandom;
+}
+
+function randomWaktu(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function munculkanBear() {
+  const tRandom = randomTanah(tanah);
+  const wRandom = randomWaktu(300, 1000);
+  tRandom.classList.add('muncul');
+
+  setTimeout(() => {
+    tRandom.classList.remove('muncul');
+    if (!selesai) {
+      munculkanBear();
+    }
+  }, wRandom);
+}
+
+function mulai() {
+  selesai = false;
+  skor = 0;
+  papanSkor.textContent = 0;
+  munculkanBear();
+  setTimeout(() => {
+    selesai = true;
+  }, 10000);
+}
+
+function pukul() {
+  skor++;
+  this.parentNode.classList.remove('muncul');
+  pop.play();
+  papanSkor.textContent = skor;
+}
+
+bear.forEach(t => {
+  t.addEventListener('click', pukul);
+});
